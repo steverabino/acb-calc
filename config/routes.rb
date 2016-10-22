df9b0ab1c07c61a84779fceaede9632353ea57cb
Steve Smith <stevesmith2609@gmail.com>
2016-10-22 17:17:24 +0100

    Adds an About ACB page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :medicines
 
   root 'welcome#index'
+
+  get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
