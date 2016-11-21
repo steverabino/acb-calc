91b57e4a4f77cf2e9f78fb31f902bfd5b68d7757
Steve Smith <stevesmith2609@gmail.com>
2016-11-21 22:14:10 +0000

    Create a Google Analytics function

--- app/assets/javascripts/google-analytics.js
+++ app/assets/javascripts/google-analytics.js
@@ -1,14 +1,53 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-87675193-1', 'auto');
+this.GoogleAnalytics = (function() {
+  function GoogleAnalytics() {}
 
-if (document.domain.indexOf("local") == -1) {
-  document.addEventListener('turbolinks:load', function(event) {
-    if (typeof ga === 'function') {
-      ga('set', 'location', event.data.url);
-      return ga('send', 'pageview');
+  GoogleAnalytics.load = function() {
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);
+      };
+      i[r].l = 1 * new Date;
+      a = s.createElement(o);
+      m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', GoogleAnalytics.analyticsId(), 'auto');
+
+    if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+      return document.addEventListener("turbolinks:load", (function() {
+        return GoogleAnalytics.trackPageview();
+      }), true);
+    } else {
+      return GoogleAnalytics.trackPageview();
     }
-  });
-};
+  };
+
+  GoogleAnalytics.trackPageview = function(url) {
+    if (!GoogleAnalytics.isLocalRequest()) {
+      return ga('send', {
+        hitType: 'pageview',
+        page: location.pathname
+      });
+    }
+  };
+
+  GoogleAnalytics.isLocalRequest = function() {
+    return GoogleAnalytics.documentDomainIncludes("local");
+  };
+
+  GoogleAnalytics.documentDomainIncludes = function(str) {
+    return document.domain.indexOf(str) !== -1;
+  };
+
+  GoogleAnalytics.analyticsId = function() {
+    return 'UA-87675193-1';
+  };
+
+  return GoogleAnalytics;
+
+})();
+
+GoogleAnalytics.load();
