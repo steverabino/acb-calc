7bfcffda02929e81a840aa7416d653d16d97a4fe
Steve Smith <stevesmith2609@gmail.com>
2016-12-15 21:55:36 +0000

    Adds peggy image to offline cache

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       cache ActionController::Base.helpers.asset_path("application.js")
       cache ActionController::Base.helpers.asset_path("proximanova-regular.otf")
       cache ActionController::Base.helpers.asset_path("bootstrap.min.js")
-      cache ActionController::Base.helpers.image_path("grandma.jpg")
+      cache ActionController::Base.helpers.image_path("peggy.jpg")
       cache ActionController::Base.helpers.image_path("icon.png")
       cache ActionController::Base.helpers.image_path("logo.png")
 
