9de450da250655d3cf4ef23fbc07ab8d7f0348d6
Steve Smith <stevesmith2609@gmail.com>
2016-12-15 21:40:55 +0000

    Precompile bootstrap and add to offline cache

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     offline = Rack::Offline.configure :cache_interval => 120 do
       cache ActionController::Base.helpers.asset_path("application.css")
       cache ActionController::Base.helpers.asset_path("application.js")
+      cache ActionController::Base.helpers.asset_path("bootstrap.min.js")
       cache ActionController::Base.helpers.image_path("grandma.jpg")
       cache ActionController::Base.helpers.image_path("icon.png")
       cache ActionController::Base.helpers.image_path("logo.png")
