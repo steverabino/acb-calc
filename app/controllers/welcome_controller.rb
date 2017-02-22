b6bdb18c8d207152e83dac5de53098df39524a58
Steve Smith <stevesmith2609@gmail.com>
2017-02-23 01:15:21 +1100

    Get twitter typeahead up and running

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,22 @@ class WelcomeController < ApplicationController
     @medicines = Medicine.all
     @brands = Brand.all
   end
+
+  def autocomplete
+    medicines = []
+    Medicine.all.each do |medicine|
+      medicines << { name: medicine.name, score: medicine.score, medicine: medicine.name, brands: medicine.brands_list }
+    end
+    Brand.all.each do |brand|
+      medicines << { name: brand.name, score: brand.medicine.score, medicine: brand.medicine.name, brands: brand.medicine.brands_list }
+    end
+
+    medicines = medicines.sort_by { |k| k[:name] }
+
+    a = medicines.map{|s| { name: s[:name], score: s[:score], medicine: s[:medicine], brands: s[:brands] }}
+    a = a.to_json
+
+    render json: a
+
+  end
 end
